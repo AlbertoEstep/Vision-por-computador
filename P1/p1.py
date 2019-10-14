e35d5e830f333e7d8b746ab1a20f45f4f35cc4a6
AlbertoEstep <albertoestep@gmail.com>
2019-10-14 19:37:20 +0200

    Cambios 1.A, 1.B

--- P1/p1.py
+++ P1/p1.py
@@ -5,34 +5,14 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-
-
-
 ####################################################### FUNCIONES P0 ##########################
 
 # Funcion para leer las imagenes
 def lee_imagen(file_name, flag_color):
-    # flag_color 0 = Gris
-    # flag_color 1 = Color
     if flag_color == 0 or flag_color == 1:
         return cv2.imread(file_name, flag_color)
     else:
         sys.exit('Imagen no valida')
-
-# Funcion para pintar las imagenes
-def pinta_imagen_cv2(imagen, titulo):
-    cv2.namedWindow(titulo, cv2.WINDOW_NORMAL)
-    cv2.imshow(titulo, imagen)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def pinta_imagen(imagen, titulo):
-    normalizar_imagen(imagen)
-    imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
-    plt.figure(0).canvas.set_window_title(titulo)
-    plt.imshow(imagen)
-    plt.show()
-
 
 def normalizar_imagen(imagen):
     if len(imagen.shape) == 2:
@@ -51,23 +31,12 @@ def normalizar_imagen(imagen):
     else:
         sys.exit('Imagen no valida')
 
-# Funcion para pintar varias imagenes a la vez
-def pintar_multiples_imagenes(secuencia_imagenes, posicion):
-    # Primero igualamos el tamaño de todas las imagenes
-    secuencia_imagenes = igualTam(secuencia_imagenes)
-
-    for i in range(len(secuencia_imagenes)):
-        # Si la imagen es monobanda (gris) se transforma en imagen tribanda (BGR)
-        if (np.size(np.shape(secuencia_imagenes[i])) == 2):
-            secuencia_imagenes[i] = cv2.cvtColor(secuencia_imagenes[i], cv2.COLOR_GRAY2BGR)
-
-        # Juntamos todas las imagenes en una dando libertad al parametro posicion{0 = vertical, 1 = horizontal}
-        if (i == 0):
-            imagenes_juntas = secuencia_imagenes[i]
-        else:
-            imagenes_juntas = np.concatenate((imagenes_juntas, secuencia_imagenes[i]), axis = posicion)
-
-    pinta_imagen(imagenes_juntas, 'Pintar varias imagenes juntas')
+def pinta_imagen(imagen, titulo):
+    normalizar_imagen(imagen)
+    imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+    plt.figure(0).canvas.set_window_title(titulo)
+    plt.imshow(imagen)
+    plt.show()
 
 # Funcion para igualar el tamaño de las imagenes
 def igualTam(imagenes):
@@ -87,32 +56,10 @@ def igualTam(imagenes):
         imagenes[i] = cv2.resize(imagen, (minimo_filas, minimo_columnas))
     return imagenes
 
-
-# Función que modifica el color de los pixeles de una imagen
-def modificar_color_pixel(imagen, pixeles, tipo_imagen, color):
-    if (tipo_imagen == 0 or tipo_imagen == 1):
-        img = lee_imagen(imagen, tipo_imagen)
-        # Si la imagen es monobanda (gris) se transforma en imagen tribanda (BGR)
-        if (tipo_imagen == 0):
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        y, x, z = np.shape(img)
-
-        # Para cada pixel de la lista lo pintamos del color pedido
-        for p in pixeles:
-            if p[0] < y and p[0] >= 0 and p[1] < x and p[1] >= 0:
-                img[p[1]][p[0]] = color
-            else:
-                sys.exit('Pixel no valido')
-        pinta_imagen(img, 'Pixeles modificados')
-    else:
-        sys.exit('Imagen no valida')
-
-
-
 # Funcion para pintar varias imagenes a la vez con pyplot
 def pintar_multiples_imagenes_pyplot(secuencia_imagenes, tituloImagen, filas, columnas, nombre):
     # Primero igualamos el tamaño de todas las imagenes
-    secuencia_imagenes = igualTam(secuencia_imagenes)
+    # secuencia_imagenes = igualTam(secuencia_imagenes)
     fig = plt.figure(0)
     fig.canvas.set_window_title(nombre)
 
@@ -128,22 +75,77 @@ def pintar_multiples_imagenes_pyplot(secuencia_imagenes, tituloImagen, filas, co
             plt.xticks([])
             plt.yticks([])
     plt.show()
-##################################################################################################################
+#######################################################################################################
 
+####################################################### FUNCIONES Auxiliares ##########################
+'''
+# No funciona
+def pintar_multiples_imagenes_en_una(secuencia_imagenes, titulo):
+    height, weight, z = secuencia_imagenes[0].shape
+    # Se crea la nueva Imagen inicializada a ceros
+    imagen = np.zeros((height*len(secuencia_imagenes), weight*len(secuencia_imagenes)),  np.uint8)
+    # Se concatenan las Imagenes
+    for i in range(len(secuencia_imagenes)):
+        imagen[:height, :weight] = secuencia_imagenes[i]
+    pinta_imagen(imagen, titulo)
+
+'''
+#######################################################################################################
+
+
+
+'''
+#NO NOS DEJAN USARLA
 # El cálculo de la convolución de una imagen con una máscara 2D. Usar una Gaussiana 2D (GaussianBlur)
 def gaussian_blur(img, size = (0, 0), sigma = 0, border = cv2.BORDER_DEFAULT):
 	return cv2.GaussianBlur(img, size, sigma, border)
+'''
 
 
-# El cálculo de la convolución de una imagen con máscaras 1D dadas por getDerivKernels
-def derive_convolution(derivX = 0, derivY = 0, size = 7, normal = True):
-	#Ksize = 1, 3, 5, 7
-    if ((size == 1) or (size == 3) or (size == 5) or (size == 7)):
-        return cv2.getDerivKernels(dx = derivX, dy = derivY, ksize = size, normalize = normal, ktype = cv2.CV_64F)
-    #Si el Ksize no es valido se obtiene error
+'''
+El cálculo de la convolución de una imagen con una máscara 2D.
+    Parametros:
+        imagen - imagen
+        ksize – Tamaño de la abertura, debe ser impar y positivo.
+        sigma – Desviación estándar gaussiana. Si no es positivo, se calcula a partir de ksize como sigma = 0.3 * ((ksize-1) * 0.5 - 1) + 0.8.
+        border - tratamiento del borde de la imagen
+
+Podemos observar que a mayor sigma, mayor difuminada queda la imagen. Análogo con el ksize. Podemos tambien poner un sigma grande en algun eje y
+el suavizado que se obtiene se aprecia mayor en el eje que tenga el sigma más grande.
+'''
+def gaussian_blur(imagen, ksize, sigma = 0, border = cv2.BORDER_DEFAULT):
+    # La función getGaussianKernel calcula y devuelve la matriz de coeficientes de filtro gaussianos
+    gaussian = cv2.getGaussianKernel(ksize, sigma)
+    # La función aplica un filtro lineal arbitrario a una imagen.
+    imagen = cv2.filter2D(imagen, -1, gaussian, anchor=(-1,-1), borderType = border)
+    return imagen
+
+
+'''
+El cálculo de la convolución de una imagen con máscaras 1D dadas por getDerivKernels
+    Parametros:
+        imagen - imagen
+        ksize – Tamaño de la abertura, debe ser impar y positivo.
+        sigma – Desviación estándar gaussiana. Si no es positivo, se calcula a partir de ksize como sigma = 0.3 * ((ksize-1) * 0.5 - 1) + 0.8.
+        border - tratamiento del borde de la imagen
+
+Podemos observar que a mayor sigma, mayor difuminada queda la imagen. Análogo con el ksize. Podemos tambien poner un sigma grande en algun eje y
+el suavizado que se obtiene se aprecia mayor en el eje que tenga el sigma más grande.
+'''
+def convolucion1D(imagen, dX = 0, dY = 0, ksize = 7, normal = True, border = cv2.BORDER_REPLICATE):
+    if ((ksize == 1) or (ksize == 3) or (ksize == 5) or (ksize == 7)):
+        # La función calcula y devuelve los coeficientes de filtro para derivadas de imágenes espaciales.
+        derivada = cv2.getDerivKernels(dx = dX, dy = dY, ksize = ksize, normalize = normal, ktype = cv2.CV_64F)
+        # La función aplica un filtro lineal separable a la imagen.
+        return cv2.sepFilter2D(imagen, -1, derivada[0], derivada[1], border)
     else:
-        print('El tamaño debe ser 1, 3, 5 o 7')
-        sys.exit()
+        sys.exit('El ksize debe ser 1, 3, 5 o 7')
+
+def maskDerivKernels(img,dx = 1,dy = 1,ksize = 3,border = cv2.BORDER_REPLICATE):
+    """
+    """
+    dxdy = cv2.getDerivKernels(dx,dy,ksize)
+    return cv2.sepFilter2D(img,-1,dxdy[0],dxdy[1],border)
 
 # Usar la función Laplacian para el cálculo de la convolución 2D con una máscara normalizada de Laplaciana-de-Gaussiana de tamaño variable.
 def laplacian_gaussian(img, sigma = 0, border = cv2.BORDER_DEFAULT, size = (0, 0), k_size = 3, depth = 0, scaler = 1, delt = 50):
@@ -191,35 +193,32 @@ def laplacian_pyramid(img, level = 4, border = cv2.BORDER_DEFAULT):
     return result
 
 def ejercicio1():
+
+
     # Ejercicio 1 A 1
-	imagen = lee_imagen('imagenes/cat.bmp', 1)
-	pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, (5, 5), 3)], ['cat', 'cat_gaussian_blur'], 1, 2, 'GaussianBlur')
-	pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, (11, 11), 6)], ['cat', 'cat_gaussian_blur(11,11),6'], 1, 2, 'GaussianBlur')
-	pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, (11, 11), 6), gaussian_blur(imagen, (23, 23), 7)], ['cat', 'cat_g_b(11,11),6', 'cat_g_b(23, 23), 7'], 1, 3, 'GaussianBlur')
-	imagen = lee_imagen('imagenes/cat.bmp', 0)
-	pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, (11, 11), 6), gaussian_blur(imagen, (23, 23), 7)], ['cat', 'cat_g_b(11,11),6', 'cat_g_b(23, 23), 7'], 1, 3, 'GaussianBlur')
+    imagen = lee_imagen('imagenes/cat.bmp', 1)
+    pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, 7, 1)], ['cat', 'blur_ksize7_sigma1'], 1, 2, 'GaussianBlur')
+    pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, 7, 7), gaussian_blur(imagen, 7, 11), gaussian_blur(imagen, 7, 21) ], ['cat', 'blur_ksize7_sigma7', 'blur_ksize7_sigma11', 'blur_ksize7_sigma21'], 2, 2, 'GaussianBlur')
+    pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, 5, 7), gaussian_blur(imagen, 9, 7), gaussian_blur(imagen, 15, 7) ], ['cat', 'blur_ksize5_sigma7', 'blur_ksize9_sigma7', 'blur_ksize15_sigma7'], 2, 2, 'GaussianBlur')
+    pintar_multiples_imagenes_pyplot([imagen, gaussian_blur(imagen, 7, 7, cv2.BORDER_CONSTANT), gaussian_blur(imagen, 7, 7, cv2.BORDER_REFLECT), gaussian_blur(imagen, 7, 7) ], ['cat', 'blur_border_constant', 'blur_border_reflect', 'blur_default'], 2, 2, 'GaussianBlur')
+
 
 	# Ejercicio 1 A 2
-	print("Para Sigma = 1\n")
-	X1, Y1 = derive_convolution(1, 1, 1)
-	X2, Y2 = derive_convolution(2, 2, 1)
-	print("Primera Derivada en X:\n", X1, "\nPrimera Derivada en Y:\n", Y1)
-	print("Segunda Derivada en X:\n", X2, "\nSegunda Derivada en Y:\n", Y2)
-	print("Para Sigma = 3\n")
-	X1, Y1 = derive_convolution(1, 1, 3)
-	X2, Y2 = derive_convolution(2, 2, 3)
-	print("Primera Derivada en X:\n", X1, "\nPrimera Derivada en Y:\n", Y1)
-	print("Segunda Derivada en X:\n", X2, "\nSegunda Derivada en Y:\n", Y2)
+    pintar_multiples_imagenes_pyplot([imagen, convolucion1D(imagen,1, 1, 1), convolucion1D(imagen, 2, 2, 1)], ['original','convolución1D', 'convolución1D'], 1, 3, 'convolución')
 
+    '''
     # Ejercicio 1 B
 	imagen = lee_imagen('imagenes/cat.bmp', 1)
 	pintar_multiples_imagenes_pyplot([imagen, laplacian_gaussian(imagen, 1, cv2.BORDER_REPLICATE), laplacian_gaussian(imagen, 1, cv2.BORDER_REFLECT)], ['Original', '1 - Replicate', '1 - Reflect'], 1, 3, 'Laplacian')
 	pintar_multiples_imagenes_pyplot([imagen, laplacian_gaussian(imagen, 3, cv2.BORDER_REPLICATE), laplacian_gaussian(imagen, 3, cv2.BORDER_REFLECT)], ['Original', '3 - Replicate', '3 - Reflect'], 1, 3, 'Laplacian')
 
+    '''
 def main():
-    """
+
     ejercicio1()
-    """
+
+
+    '''
     # Ejercicio 2 A
     imagen = lee_imagen('imagenes/cat.bmp', 0)
     pinta_imagen(gaussian_pyramid(imagen), "Piramide gaussiana")
@@ -227,7 +226,7 @@ def main():
     # Ejercicio 2 B
     imagen = lee_imagen('imagenes/cat.bmp', 0)
     pinta_imagen(laplacian_pyramid(imagen), "Piramide Laplaciana")
-
+    '''
 
 
 
