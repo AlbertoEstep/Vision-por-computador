612033ff6d727f915f2a8a8b9a4d8994dbcb996a
AlbertoEstep <albertoestep@gmail.com>
2019-12-19 18:05:19 +0100

    Ej 4

--- P3/p3.py
+++ P3/p3.py
@@ -171,7 +171,7 @@ def get_puntos_harris(imagen, dx, dy, block_size = 5, ksize = 3, threshold = 10,
     return maximos
 
 
-def get_matches_bf_cc(imagen1, imagen2, n = 100, flag = 2):
+def get_matches_bf_cc(imagen1, imagen2, n = 100, flag = 2, pintar = True):
     # Se obtienen los keypoints y los descriptores de las dos imágenes
     akaze = cv2.AKAZE_create()
     kpts1, desc1 = akaze.detectAndCompute(imagen1, None)
@@ -205,10 +205,13 @@ def get_matches_bf_cc(imagen1, imagen2, n = 100, flag = 2):
 
     #matches = sorted(matches, key = lambda x:x.distance)[0:n]
     matches = random.sample(matches, n)
-    return cv2.drawMatches(imagen1, kpts1, imagen2, kpts2, matches, None, flags = flag)
+    if pintar:
+        cv2.drawMatches(imagen1, kpts1, imagen2, kpts2, matches, None, flags = flag)
+    else:
+        return matches
 
 
-def get_matches_knn(imagen1, imagen2, k = 2, ratio = 0.8, n = 100, flag = 2):
+def get_matches_knn(imagen1, imagen2, k = 2, ratio = 0.8, n = 100, flag = 2, pintar = True):
     akaze = cv2.AKAZE_create()
     kpts1, desc1 = akaze.detectAndCompute(imagen1, None)
     kpts2, desc2 = akaze.detectAndCompute(imagen2, None)
@@ -220,12 +223,68 @@ def get_matches_knn(imagen1, imagen2, k = 2, ratio = 0.8, n = 100, flag = 2):
             good.append([p1])
     #matches = sorted(good, key = lambda x:x[0].distance)
     matches = random.sample(good, n)
-    return cv2.drawMatchesKnn(imagen1, kpts1, imagen2, kpts2, matches, None, flags = flag)
+    if pintar:
+        cv2.drawMatchesKnn(imagen1, kpts1, imagen2, kpts2, matches, None, flags = flag)
+    else:
+        return kpts1, desc1, kpts2, desc2, matches
+
+def get_homografia(imagen1, imagen2):
+    kpts1, desc1, kpts2, desc2, matches = get_matches_knn(imagen1, imagen2, pintar = False)
+    puntos_origen = np.float32([kpts1[punto[0].queryIdx].pt for punto in matches]).reshape(-1, 1, 2)
+    puntos_destino = np.float32([kpts2[punto[0].trainIdx].pt for punto in matches]).reshape(-1, 1, 2)
+    homografia , _ = cv2.findHomography(puntos_origen, puntos_destino, cv2.RANSAC, 1)
+    return homografia
+
+def homografia_identidad(imagen, ancho_mosaico, alto_mosaico):
+    """
+    Calcula la homografía necesaria para llevar una imagen (imagen) al centro de
+    un mosaico de tamaño (ancho_mosaico x alto_mosaico)
+    """
+    tx = ancho_mosaico/2 - imagen.shape[0]/2
+    ty = alto_mosaico/2 - imagen.shape[1]/2
+    return np.array([[1, 0, tx], [0, 1, ty], [0, 0, 1]], dtype=np.float32)
+
+def get_mosaicoN(*args):
+    # Se calcula cuál es el número de la imagen que se encuentra en el centro
+    index_img_center = int(len(args)/2)
+    # Se obtiene la imagen que está en el centro
+    img_center =  args[index_img_center]
+    ancho_mosaico = sum([img.shape[1] for img in args])
+    alto_mosaico = args[0].shape[0]*2
+    homografia_id = homografia_identidad(img_center, ancho_mosaico, alto_mosaico)
+    img = cv2.warpPerspective(img_center, homografia_id, (ancho_mosaico, alto_mosaico), borderMode=cv2.BORDER_TRANSPARENT)
+    homographies = [None] * len(args)
+    homographies[index_img_center] = homografia_id
+    for i in range(0, index_img_center)[::-1]:
+        m = get_homografia(args[i], args[i+1])
+        m = np.dot(homographies[i+1], m)
+        homographies[i] = m
+        img = cv2.warpPerspective(args[i], m, (ancho_mosaico, alto_mosaico), dst=img, borderMode=cv2.BORDER_TRANSPARENT)
+    for i in range(index_img_center+1, len(args)):
+        m = get_homografia(args[i], args[i-1])
+        m = np.dot(homographies[i-1], m)
+        homographies[i] = m
+        img = cv2.warpPerspective(args[i], m, (ancho_mosaico, alto_mosaico), dst=img, borderMode=cv2.BORDER_TRANSPARENT)
+    #img = crop_image(img)
+    return img
+
+
+def crop_image(img):
+    """
+    Recorta todos los bordes negros a una imagen y la devuelve.
+    Código completamente obtenido de:
+    - https://stackoverflow.com/questions/13538748/crop-black-edges-with-opencv
+    """
+    img = img.astype(np.uint8)
+    _, thresh = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY)
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    x, y, w, h = cv2.boundingRect(contours)
+    return img[y:y+h,x:x+w]
 
 def apartado1AB():
     # Parametros
-    #im = lee_imagen('imagenes/yosemite/Yosemite1.jpg')
-    im = lee_imagen('imagenes/Tablero1.jpg')
+    im = lee_imagen('imagenes/yosemite/Yosemite1.jpg')
+    #im = lee_imagen('imagenes/Tablero1.jpg')
     imagen = np.copy(im)
 
     #imagen = imagen.astype(np.uint8)
@@ -247,13 +306,11 @@ def apartado1AB():
         puntos_harris += puntos
         print("Numero de puntos harris encontrados en la octava " + str(i+1) + ": " + str(len(puntos)))
         imagen_key = cv2.drawKeypoints(imagen, puntos, np.array([]), color = (0,0,255), flags = 4)
-        #imagen_key = pinta_circulos(imagen, puntos_harris)
         pinta_imagen(imagen_key)
 
     print("Numero de puntos harris encontrados en total: " + str(len(puntos_harris)))
      # Se añade la imagen a la lista de imágenes
     imagen_key = cv2.drawKeypoints(imagen, puntos_harris, np.array([]), color = (0,0,255), flags = 4)
-    #imagen_key = pinta_circulos(imagen, puntos_harris)
     pinta_imagen(imagen_key)
 
 def apartado2A():
@@ -276,21 +333,58 @@ def apartado2B():
     imagenes.append(get_matches_knn(imagen1, imagen2))
     show_images(imagenes, cols = 1)
 
+def apartado3():
+    mosaico = []
+    imagen1 = lee_imagen('imagenes/yosemite/Yosemite1.jpg')
+    imagen2 = lee_imagen('imagenes/yosemite/Yosemite2.jpg')
+    #mosaico.append(get_mosaico2(imagen1, imagen2))
+    # Se muestran las imágenes que se leen inicialmente
+    #show_images(mosaico, cols = 1)
+
+def apartado4():
+    mosaico = []
+    imagen1 = lee_imagen('imagenes/yosemite_full/yosemite1.jpg')
+    imagen2 = lee_imagen('imagenes/yosemite_full/yosemite2.jpg')
+    imagen3 = lee_imagen('imagenes/yosemite_full/yosemite3.jpg')
+    imagen4 = lee_imagen('imagenes/yosemite_full/yosemite4.jpg')
+    imagen5 = lee_imagen('imagenes/yosemite_full/yosemite5.jpg')
+    imagen6 = lee_imagen('imagenes/yosemite_full/yosemite6.jpg')
+    imagen7 = lee_imagen('imagenes/yosemite_full/yosemite7.jpg')
+    mosaico.append(get_mosaicoN(imagen1, imagen2, imagen3, imagen4))
+    # Se muestran las imágenes que se leen inicialmente
+    show_images(mosaico, cols = 1)
+
+
+
+
 def ej1():
     print("Ejercicio 1")
     apartado1AB()
-    #input("Pulsa enter para continuar")
-
+    input("Pulsa enter para continuar")
 
 def ej2():
     print("Ejercicio 2")
     apartado2A()
     input("Pulsa enter para continuar")
     apartado2B()
+    input("Pulsa enter para continuar")
+
+def ej3():
+    print("Ejercicio 3")
+    apartado3()
+    input("Pulsa enter para continuar")
+
+def ej4():
+    print("Ejercicio 4")
+    apartado4()
+    input("Pulsa enter para continuar")
+
 
 def main():
-    ej1()
+    #ej1()
     #ej2()
+    ej3()
+    ej4()
 
 
 
